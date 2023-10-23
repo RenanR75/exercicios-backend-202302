85ab628f5a9108f990392b961d52e512167a7ad1
Renan <144182548+RenanR75@users.noreply.github.com>
2023-10-23 22:26:20 +0000

    Mon, Oct 23, 2023, 7:26 PM -03:00

--- poo/projeto010/main.py
+++ poo/projeto010/main.py
@@ -10,6 +10,6 @@ def indice():
 def indiceMundo():
     return{"mensagem" : "Ola Todo Mundo."}
 
-@app.get("/marte/{nome_planeta}")
-def indiceMartee( nome_planeta: str = fastapi.Path(..., description="Preencha com o nome do planeta")):
+@app.get("/vialactea/{nome_planeta}")
+def indiceViaLactea( nome_planeta: str = fastapi.Path(..., description="Preencha com o nome do planeta")):
     return{"mensagem": "Ola {nome_planeta}".format(nome_planeta)}
