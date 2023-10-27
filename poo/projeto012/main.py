98852bb946b81fe38b018e53cc729703e5a30c7f
Renan <144182548+RenanR75@users.noreply.github.com>
2023-10-27 23:42:22 +0000

    Fri, Oct 27, 2023, 8:42 PM -03:00

--- poo/projeto012/main.py
+++ poo/projeto012/main.py
@@ -4,6 +4,17 @@ app = fastapi.FastAPI()
 def hello_world_root():
     return {"Olá": "Funciona"}
 
+@app.get("/getContinents")
+def getContinents():
+    Continents_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql = "select * from continents"
+    mycursor.execute(sql)
+    for data_continents in mycursor:
+        Continents_list.append( data_continents )
+    mycursor.close()
+    return {"Continents": Continents_list}
+
 @app.get("/getContinents/{continent_id}")
 def getContinents(continent_id : int):
     Continents_list = []
