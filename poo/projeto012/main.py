a47f19450b9ee3722b22e85537e81e15412afc76
Renan <144182548+RenanR75@users.noreply.github.com>
2023-10-27 23:32:41 +0000

    Fri, Oct 27, 2023, 8:32 PM -03:00

--- poo/projeto012/main.py
+++ poo/projeto012/main.py
@@ -8,6 +8,7 @@ def hello_world_root():
 def getContinents():
     Continents_list = []
     mycursor= connection.mydb.cursor(dictionary=True)
+    mycursor.execute("select * from continents")
     for data_continents in mycursor:
         Continents_list.append(data_continents)
     mycursor.close()
