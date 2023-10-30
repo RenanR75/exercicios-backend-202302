685b875501962f1dc2d9ba8af5f6ff310a0267f1
Renan <144182548+RenanR75@users.noreply.github.com>
2023-10-30 22:07:01 +0000

    Mon, Oct 30, 2023, 7:07 PM -03:00

--- poo/projeto012/main.py
+++ poo/projeto012/main.py
@@ -27,6 +27,17 @@ def getContinents(continent_id : int):
     mycursor.close()
     return {"Continents": Continents_list}
 
+@app.get("/getRegions")
+def getRegions():
+    Regions_list = []
+    mycursor = connection.mydb.cursor(dictionary=True)
+    sql = "select * from regions"
+    mycursor.execute(sql)
+    for data_regions in mycursor:
+        Regions_list.append( data_regions )
+    mycursor.close()
+    return {"Regions": Regions_list}
+
 @app.get("/getRegion/{continent_id}")
 def getRegion(continent_id : int):
     Continents_list = []
