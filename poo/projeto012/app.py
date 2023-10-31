e0e0101a8c6ff2ada780a1f93410f67141a830ff
Renan <144182548+RenanR75@users.noreply.github.com>
2023-10-31 21:46:50 +0000

    Tue, Oct 31, 2023, 6:46 PM -03:00

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -5,5 +5,10 @@ app = flask.Flask(__name__)
 def OlaMundo():
     return "<p>Ola Mundo</p>"
 
+@app.route("/consultaContinentes")
+def consultaContinentes():
+    response2 = requests.get("http://127.0.0.1:8000/getContinents")
+    return response2.json()
+
 if __name__ == '__main__':
     app.run(debug=True)
