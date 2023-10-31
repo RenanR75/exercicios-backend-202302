2cc4c297f175a0b6317bbecf08c99ea17972fed6
Renan <144182548+RenanR75@users.noreply.github.com>
2023-10-31 21:56:14 +0000

    Tue, Oct 31, 2023, 6:56 PM -03:00

--- poo/projeto012/app.py
+++ poo/projeto012/app.py
@@ -8,7 +8,14 @@ def OlaMundo():
 @app.route("/consultaContinentes")
 def consultaContinentes():
     response2 = requests.get("http://127.0.0.1:8000/getContinents")
-    return response2.json()
+
+    json_object = response2.json()
+    build_direction = "LEFT_TO_RIGHT"
+    table_attributes = {"style" : "width:100%"}
+    html = json2table.convert(json_object, build_direction=build_direction, table_attributes=table_attributes)
+    #print(html)
+    #return response2.json()
+    return html
 
 if __name__ == '__main__':
     app.run(debug=True)
